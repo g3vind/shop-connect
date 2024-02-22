32424312a9dc3f11aa512b57a1430e912b9d71d8
g3vind <g3vind@gmail.com>
2024-02-22 15:42:34 +0530

    deployment changes 3

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://shop-connect-zeta.vercel.app/api/v2";
 
-export const backend_url = "https://shop-connect-zeta.vercel.app/";
+// export const backend_url = "https://shop-connect-zeta.vercel.app/";
