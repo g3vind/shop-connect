a878edf6b2ff5108226c0a5839c85ebdc362491d
g3vind <g3vind@gmail.com>
2024-02-24 16:33:55 +0530

    backend url changed

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://shop-connect-backend.vercel.app/api/v2";
 
 export const backend_url = "http://localhost:8000/";
