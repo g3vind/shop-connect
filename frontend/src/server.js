466ef94c3f9247642884ba8e5d65cd4256f41951
g3vind <g3vind@gmail.com>
2024-02-24 16:53:14 +0530

    dep changes

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://shop-connect-backend.vercel.app/api/v2";
+export const server = "https://eshop-tutorial.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://eshop-tutorial.vercel.app";
